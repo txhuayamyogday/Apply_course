6a78a6c2313748cc42c59ca8022e76f1602432f1
txhuayamyogday <147779248+txhuayamyogday@users.noreply.github.com>
2025-07-27 15:07:54 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import App from "./App.vue";
 import router from "./router";
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/reset.css";
-
+//kkk
 const app = createApp(App);
 app.use(router);
 app.use(Antd);
